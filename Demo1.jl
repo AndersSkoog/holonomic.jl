43344a420a0f9e33c20fc6f8af341d61dae3b072
Anders Skoog <anders.skooog@gmail.com>
2026-07-04 04:36:20 +0200

    .

---
+++ Demo1.jl
@@ -0,0 +1,13 @@
+using GLMakie
+using LinearAlgebra
+using .Structs
+using .CP1
+using .S2
+using .SO3
+using .Spin
+using .Connection
+using .InvariantCone:cap_circle
+
+fig = Figure()
+ax = Axis3(fig[1,1])
+
--- Spin.jl
+++ Spin.jl
@@ -1,6 +1,15 @@
 module Spin
 using .Structs:UnitSpinor
-export UnitSpinor_project
+export UnitSpinor_project, UnitSpinor_from_S2
+
+function UnitSpinor_from_S2(v::S2)
+    θ = theta(v)
+    φ = phi(v)
+    α = cos(θ/2)
+    β = exp(im * φ) * sin(θ/2)
+    return UnitSpinor(ComplexF64(α), ComplexF64(β))
+end
+
 UnitSpinor_project(s::UnitSpinor) = s.β / s.α
 end
 
